21a81ed851229f9c7313937527f245c7209acad2
ainul yaqin <ainul.yaqin>
2024-04-17 09:27:15 +0700

    no message

--- src/main/java/com/fisclouds/chart/dto/ChartDataDto.java
+++ src/main/java/com/fisclouds/chart/dto/ChartDataDto.java
@@ -3,6 +3,6 @@ package com.fisclouds.chart.dto;
 import java.util.List;
 import java.util.Map;
 
-public record ChartDataDto(List<Map<String, Object>> datasets, String columns, String chartType ) {
+public record ChartDataDto(List<Map<String, Object>> datasets, Map<String, Object> config, String columns, String chartType ) {
 
 }
